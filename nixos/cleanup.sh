33e3d4e3d998b267befc8bd14814a352e0df115d
berton7 <francy.berton99@gmail.com>
2024-03-29 16:58:13 +0100

    fix nix-collect-garbage deleting all generations

--- nixos/cleanup.sh
+++ nixos/cleanup.sh
@@ -8,10 +8,7 @@ if [ -z "$1" ]
     exit 1
 fi
 
-nix-collect-garbage  --delete-older-than $1
-
-# recommeneded to sometimes run as sudo to collect additional garbage
-sudo nix-collect-garbage -d
+sudo nix-collect-garbage  --delete-older-than $1
 
 # As a separation of concerns - you will need to run this command to clean out boot
 sudo /run/current-system/bin/switch-to-configuration boot
