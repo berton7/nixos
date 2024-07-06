c14b2ef797355ac3c599d96d06dc74817ed1a4d9
berton7 <francy.berton99@gmail.com>
2024-07-06 12:10:15 +0200

    Updated cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -4,11 +4,11 @@ set -e
 
 if [ -z "$1" ]
   then
-    echo "Missing \"--delete-older-than\" value. Example usage: \`~/dotfiles/nixos/cleanup.sh 30d\`"
-    exit 1
+    sudo nix-collect-garbage  -d
+  else
+    sudo nix-collect-garbage  --delete-older-than $1
 fi
 
-sudo nix-collect-garbage  --delete-older-than $1
 
 # As a separation of concerns - you will need to run this command to clean out boot
 sudo /run/current-system/bin/switch-to-configuration boot
