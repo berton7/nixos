bb2c889108d070e73bbbe38d3be6bb93a7f08689
berton7 <francy.berton99@gmail.com>
2024-09-25 21:58:02 +0200

    Added simple default flake configuration

---
+++ modules/home/flake_default.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    myrepo = {
+      url = "github:berton7/myflake";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, myrepo }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+          mypkgs = myrepo.packages.${system};
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell {
+            # put here your build pkgs!
+            buildInputs = [  ];
+          };
+        }
+      );
+}
