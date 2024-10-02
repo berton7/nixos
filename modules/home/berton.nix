9e8117d044042a9b902a3eb358c51e7221dc7cdc
berton7 <francy.berton99@gmail.com>
2024-10-02 20:51:53 +0200

    Managing konsole zsh profile

--- modules/home/berton.nix
+++ modules/home/berton.nix
@@ -88,19 +88,29 @@ in {
     libsForQt5.kdeconnect-kde
   ];
 
-  # Home Manager is pretty good at managing ${dotfilesRoot}. The primary way to manage
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of '${dotfilesRoot}/screenrc' in
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
-    # ".screenrc".source = ${dotfilesRoot}/screenrc;
+    # ".screenrc".source = dotfiles/screenrc;
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
     # '';
+
+    ".local/share/konsole/zsh.profile".text = "
+[Appearance]
+ColorScheme=DarkPastels
+
+[General]
+Command=zsh -l
+Name=zsh
+Parent=FALLBACK/
+";
   };
 
   # Home Manager can also manage your environment variables through
