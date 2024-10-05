d66559e44eac76c8a659086b3a0dcbd6ba78fbb0
berton7 <francy.berton99@gmail.com>
2024-10-05 21:43:51 +0200

    nixos-desktop 60 current  2024-10-05 21:43:46  24.11.20241004.bc947f5  6.6.53                          *

--- hosts/nixos-desktop/configuration.nix
+++ hosts/nixos-desktop/configuration.nix
@@ -215,8 +215,8 @@
     ntfs3g
     file
     nil
-    ];
-  
+  ];
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
